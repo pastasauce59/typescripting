1012c9407067559e7e647ba9845f405fdd8ead9c
Michael Primavera <pastasauce59@gmail.com>
2022-09-10 13:06:15 +0200

    practicing with type inference and the any type

--- Types/variables.ts
+++ Types/variables.ts
@@ -12,3 +12,44 @@ numCatLives += 1
 let gameOver: boolean = true
 gameOver = false
 gameOver = 'true'
+
+
+
+//Type inference
+let tvShow = 'Gundam Wing'
+tvShow = false
+
+let isFunny = false
+isFunny = true
+isFunny = 'asd'
+
+let once = true
+
+isFunny = once
+
+
+
+// any - the any type
+let thing: any = "hello"
+thing = 1
+thing = false
+thing = []
+thing()
+thing.toUpperCase()
+
+// ^ all extra checks from typescripy are gone with the any type - use sparringly
+
+
+
+// example of when you might actually want to use any type
+const movies = ['Arrival', 'The Thing', 'Aliens', 'Amadeus']
+let foundMovie;
+// let foundMovie: string
+
+for(let movie of movies){
+    if(movie === 'The Thing'){
+        foundMovie = movie
+    }
+}
+foundMovie()
+foundMovie = 1
